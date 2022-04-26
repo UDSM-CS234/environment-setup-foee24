6990ec7346da77f0aefa388c8bac8e97e5032e67
cza-austin <84322535+cza-austin@users.noreply.github.com>
2022-04-26 17:32:40 +0300

    This is  assignment1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        double gravity = -9.81; // Earth's gravity in m/s^2
+        double gravity = -9.810; // Earth's gravity in m/s^2
         double initialVelocity = 0.0;
         double fallingTime = 10.0;
         double initialPosition = 0.0;
